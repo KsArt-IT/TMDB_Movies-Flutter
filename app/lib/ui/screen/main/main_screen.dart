7403b65fccaa8cf50b8c223635cac0e58ccd74e2
KsArT-IT <ksart.it@gmail.com>
2025-03-29 19:01:12 +0200

    refactor: update MainScreen to use localized titles and improve navigation handling

--- app/lib/ui/screen/main/main_screen.dart
+++ app/lib/ui/screen/main/main_screen.dart
@@ -10,34 +10,53 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final s = S.of(context);
+
+    final isDetailScreen = GoRouter.of(
+      context,
+    ).routerDelegate.currentConfiguration.uri.path.contains('detail');
+
+    // final title = GoRouter.of(context).routerDelegate.state.name;
+
+    String getTitle(int index) => switch (index) {
+      0 => s.topMoviesTitle,
+      1 => s.moviesTitle,
+      2 => s.favoriteTab,
+      3 => s.settingsTab,
+      _ => s.appTitle,
+    };
+
+    List<BottomNavigationBarItem> navBarItems = <BottomNavigationBarItem>[
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: s.topTab),
+      BottomNavigationBarItem(icon: Icon(Icons.movie), label: s.moviesTab),
+      BottomNavigationBarItem(icon: Icon(Icons.favorite), label: s.favoriteTab),
+      BottomNavigationBarItem(icon: Icon(Icons.settings), label: s.settingsTab),
+    ];
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(S.of(context).topMoviesTitle),
+        backgroundColor: theme.colorScheme.inversePrimary,
+        // title: Text(title ?? ''),
+        title: Text(getTitle(navigationShell.currentIndex)),
+        leading:
+            isDetailScreen
+                ? IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () => context.pop(),
+                )
+                : null,
       ),
-      body: Center(child: Text('Home Screen')),
+      body: navigationShell,
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: theme.colorScheme.inversePrimary,
-        unselectedItemColor: theme.colorScheme.onSurface,
+        unselectedItemColor: theme.colorScheme.secondary,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
         backgroundColor: theme.colorScheme.surface,
-        items: _navBarItems,
+        items: navBarItems,
         currentIndex: navigationShell.currentIndex,
-        onTap: (index) {
-          navigationShell.goBranch(
-            index,
-            initialLocation: index == navigationShell.currentIndex,
-          );
-        },
+        onTap: (int index) => navigationShell.goBranch(index),
       ),
     );
   }
-
-  List<BottomNavigationBarItem> get _navBarItems =>
-      const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Business'),
-        BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'School'),
-      ];
 }
