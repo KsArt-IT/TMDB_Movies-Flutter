5fafc964e8b12a6cd6637fe1f6d5d74545f3948e
KsArT-IT <ksart.it@gmail.com>
2025-03-29 18:59:55 +0200

    feat: enhance routing with detailed screens for Top Movies, Movies, Favorites, and Settings

--- app/lib/core/router/app_router.dart
+++ app/lib/core/router/app_router.dart
@@ -12,16 +12,60 @@ final GoRouter router = GoRouter(
         StatefulShellBranch(
           routes: [
             GoRoute(
+              name: "Top Movies",
               path: '/top_movies',
               builder: (context, state) => const TopMoviesScreen(),
+              routes: [
+                  GoRoute(
+                    path: 'detail_top/:movieId',
+                    builder: (context, state) => DetailMovieScreen(id: state.pathParameters['movieId'] ?? ''),
+                  ),
+                ],
             ),
           ],
         ),
         StatefulShellBranch(
           routes: [
             GoRoute(
+              name: "Movies",
               path: '/movies',
               builder: (context, state) => const MoviesScreen(),
+              routes: [
+                GoRoute(
+                  path: 'detail_movie/:movieId',
+                  builder:
+                      (context, state) => DetailMovieScreen(
+                        id: state.pathParameters['movieId'] ?? '',
+                      ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              name: 'Favorites',
+              path: '/favorites',
+              builder: (context, state) => const FavoriteMoviesScreen(),
+              routes: [
+                GoRoute(
+                  path: 'detail_favorite/:movieId',
+                  builder:
+                      (context, state) => DetailMovieScreen(
+                        id: state.pathParameters['movieId'] ?? '',
+                      ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              name: "Settings",
+              path: '/settings',
+              builder: (context, state) => const SettingsScreen(),
             ),
           ],
         ),
