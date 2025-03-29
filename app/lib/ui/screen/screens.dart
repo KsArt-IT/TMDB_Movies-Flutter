2d07df68e3c24e66e8c0a26d65c00e4eb4bfe088
KsArT-IT <ksart.it@gmail.com>
2025-03-29 19:00:10 +0200

    feat: add screens export for Main, Top Movies, Movies, Favorites, Settings, and Detail Movie

---
+++ app/lib/ui/screen/screens.dart
@@ -0,0 +1,8 @@
+export 'package:tmdb_movies/ui/screen/main/main_screen.dart';
+
+export 'package:tmdb_movies/ui/screen/top_movies/top_movies_screen.dart';
+export 'package:tmdb_movies/ui/screen/movies/movies_screen.dart';
+export 'package:tmdb_movies/ui/screen/favorite_movies/favorite_movies_screen.dart';
+export 'package:tmdb_movies/ui/screen/settings/settings_screen.dart';
+
+export 'package:tmdb_movies/ui/screen/detail_movie/detail_movie_screen.dart';
